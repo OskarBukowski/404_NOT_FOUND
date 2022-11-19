9575521ba19928138edf8f78812c3a1e3f9d5f41
lzagorski <lukaszzagorski99@gmail.com>
2022-11-19 12:14:44 +0100

    Initial commit

---
+++ users/models.py
@@ -0,0 +1,44 @@
+from django.contrib.auth.base_user import AbstractBaseUser, BaseUserManager
+from django.contrib.auth.models import PermissionsMixin
+from django.core.exceptions import ValidationError
+from django.core.validators import validate_email
+from django.db import models
+
+
+class UserManager(BaseUserManager):
+
+    def create_user(self, email, password):
+        if not email:
+            raise ValueError("Please specify email for account")
+        try:
+            validate_email(email)
+        except ValidationError:
+            raise ValueError("Please check if email is correct")
+
+        user = self.model(email=email, password=None)
+        user.set_password(password)
+        user.save()
+        return user
+
+    def create_superuser(self, email, password):
+        user = self.create_user(email, password)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save()
+        return user
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+
+    email = models.EmailField(max_length=128, unique=True)
+    creation_date = models.DateTimeField(auto_now_add=True)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+
+    objects = UserManager()
+
+    REQUIRED_FIELDS = ['email']
+
+    def __str__(self):
+        return self.email
