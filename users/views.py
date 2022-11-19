bf1335a5957df27889ff604e0a2bc76f0c058f67
lzagorski <lukaszzagorski99@gmail.com>
2022-11-19 12:43:08 +0100

    Base templates

--- users/views.py
+++ users/views.py
@@ -1,3 +1,38 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import Group
+from django.shortcuts import render, redirect
 
-# Create your views here.
+from users.forms import UserCreateForm, UserLoginForm
+
+
+def home_view(request):
+    return render(request, 'homepage.html')
+
+
+def register_view(request):
+    form = UserCreateForm()
+    if request.method == 'POST':
+        form = UserCreateForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            user.groups.add(Group.objects.get(name='User'))
+            user.save()
+            return redirect('login')
+    return render(request, 'register.html', context={'form': form})
+
+
+def login_view(request):
+    form = UserLoginForm()
+    if request.method == 'POST':
+        form = UserLoginForm(request, request.POST)
+        if form.is_valid():
+            user = authenticate(request, username=form.data['username'], password=form.data['password'])
+            if user.is_authenticated:
+                login(request, user)
+                return redirect('home')
+    return render(request, 'login.html', context={'form': form})
+
+
+def logout_view(request):
+    logout(request)
+    return render(request, 'homepage.html')
