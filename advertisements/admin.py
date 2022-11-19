ea0a7226e0ebc7aa66e077153e8fd6683718a1f0
ccxAlpha <oskar.bukowski1@gmail.com>
2022-11-19 13:34:14 +0100

    .

---
+++ advertisements/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+
+from advertisements.models import Advert, Category
+
+# Register your models here.
+
+admin.site.register(Advert)
+admin.site.register(Category)
---
+++ advertisements/models.py
@@ -0,0 +1,41 @@
+from django.db import models
+
+from users.models import User
+
+
+#ADVERT:
+"""
+Sender
+Post date
+if active
+City
+Phone number
+picture
+Category
+Descripiton
+
+"""
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=128)
+
+
+class Advert(models.Model):
+    post_date = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    is_active = models.BooleanField(default=True)
+    city = models.CharField(max_length=128)
+    phone = models.IntegerField()
+    picture = models.ImageField()
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    description = models.TextField()
+
+
+
+
+
+
+
+
+
