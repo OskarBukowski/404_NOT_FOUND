390a129beed7712e1404959b7a74d85732265720
ccxAlpha <oskar.bukowski1@gmail.com>
2022-11-19 18:22:25 +0100

    added path to pictures

--- project/settings.py
+++ project/settings.py
@@ -31,6 +31,9 @@ ALLOWED_HOSTS = ['localhost', '3.10.192.63', '127.0.0.1']
 
 LOGIN_URL = '/login'
 
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+MEDIA_URL = "/media/"
+
 
 # Application definition
 
