a1c7156847dcd989bd39984ebeb3a4cbb6e5a4bf
ccxAlpha <oskar.bukowski1@gmail.com>
2022-11-19 14:33:29 +0100

    added single advert view

--- advertisements/views.py
+++ advertisements/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
-
 from advertisements.models import Advert
 
 
 def list_view(request):
     adverts = Advert.objects.all()
     return render(request, 'adverts.html', context={'adverts': adverts})
+
+
+
+def single_advert(request, id):
+    advert = Advert.objects.get(pk=id)
+    return render(request, 'advert_detail.html', context={'advert': advert})
+
